b7c2e18d6b4853d039cefb7acf9b57aaa6959c12
CHU hashimoto <hasimt.t@chunichi.co.jp>
2021-06-17 19:50:20 +0900

    test

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 git switch chu/shuppan
 git add C:\Users\chunichi C:\Users\chunichi\Desktop\backnumber_add"\backnumber.dat
-git commit -m "テストコミット"
+git commit -m "test commit"
 git switch test
 git merge origin/chu/shuppan
 git push origin test
