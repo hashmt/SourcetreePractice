41a97c2740bba365374077089191ebbae0ac76d2
CHU hashimoto <hasimt.t@chunichi.co.jp>
2021-06-17 19:30:33 +0900

    test

--- test.sh
+++ test.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-git checkout {shuppan}
-git add {C:\Users\chunichi\Documents\SourcetreePractice\test_html_cgi\test_config\backnumber.dat}
-git commit -m "{ƒeƒXƒg‚Å’Ç‰Á}"
+git switch shuppan
