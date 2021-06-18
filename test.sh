16a87de1d35c659fb255a275f8356afe78250597
CHU hashimoto <hasimt.t@chunichi.co.jp>
2021-06-18 15:27:54 +0900

    test

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 git checkout chu/shuppan
-git add C:\Users\chunichi\Documents\SourcetreePractice\test\dragons\backnumber.dat
+git add C:/Users\chunichi/Documents/SourcetreePractice/test/dragons/backnumber.dat
 
-$ct="$(date +'%Y:%m:%d-%H:%M:%S')"
+ct="$(date +'%Y:%m:%d-%H:%M:%S')"
 
-git commit -m $ct
+git commit -m ct
 git checkout test
 git pull origin test
 git merge chu/shuppan
