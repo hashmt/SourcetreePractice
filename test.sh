94268dfb002d927c2b9098f78213e33120f6be99
CHU hashimoto <hasimt.t@chunichi.co.jp>
2021-06-18 15:12:09 +0900

    test

--- test.sh
+++ test.sh
@@ -2,7 +2,10 @@
 
 git checkout chu/shuppan
 git add C:\Users\chunichi C:\Users\chunichi\Desktop\backnumber_add"\backnumber.dat
-git commit -m "test commit"
+
+$ct="$(date +'%Y:%m:%d-%H:%M:%S')"
+
+git commit -m $ct
 git checkout test
 git pull origin test
 git merge chu/shuppan
