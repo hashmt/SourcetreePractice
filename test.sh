3ac61c627ba0ac75e1ab60e0886dceb353e1826e
CHU hashimoto <hasimt.t@chunichi.co.jp>
2021-06-18 15:52:29 +0900

    test

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 git checkout chu/shuppan
 git add C:/Users/chunichi/Documents/SourcetreePractice/test/dragons/backnumber.dat
 
-git commit -m  dragonsFbacknumber
+git commit -m "dragonsFbacknumber"
 git checkout test
 git pull origin test
 git merge chu/shuppan
